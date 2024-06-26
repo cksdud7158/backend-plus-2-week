bbd6f4ec77006583368bd465eb92ed4f5b17dc05
cksdud7158 <cksdud7158@naver.com>
2024-06-27 02:27:11 +0900

    feature :  registrationHistory repository 기능 구현

---
+++ src/lectures/repository/registrationHistory.repository.interface.ts
@@ -0,0 +1,11 @@
+import { RegistrationHistory } from "../entity/registrationHistory.entity";
+
+export const IREGISTRATION_HISTORY_REPOSITORY =
+  "IRegistrationHistoryRepository";
+
+export interface IRegistrationHistoryRepository {
+  findByUserIdAndDate(
+    userId: number,
+    registrationDate: string,
+  ): Promise<RegistrationHistory[]>;
+}
---
+++ src/lectures/repository/registrationHistory.repository.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@nestjs/common";
+import { IRegistrationHistoryRepository } from "./registrationHistory.repository.interface";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { RegistrationHistory } from "../entity/registrationHistory.entity";
+
+@Injectable()
+export class RegistrationHistoryRepositoryImpl
+  implements IRegistrationHistoryRepository
+{
+  constructor(
+    @InjectRepository(RegistrationHistory)
+    private readonly registrationHistory: Repository<RegistrationHistory>,
+  ) {}
+
+  async findByUserIdAndDate(
+    userId: number,
+    registrationDate: string,
+  ): Promise<RegistrationHistory[]> {
+    return await this.registrationHistory.find({
+      relations: {
+        lectureSchedule: true,
+      },
+      where: {
+        userId: userId,
+        lectureSchedule: {
+          lectureDate: registrationDate,
+        },
+      },
+    });
+  }
+}
