2bf6d61817f35e48ed4d679405f1fb6a3a7406cd
cksdud7158 <cksdud7158@naver.com>
2024-06-27 22:27:06 +0900

    feature : registrationHistory repository 메서드 추가 fix : findByUserIdAndDate -> findByUserIdAndLectureIdAndDate 로 변경하여 lectureId 를 받아 해당 날짜에 해당 강의에 해당 유저가 신청했는지 조회하도록 수정

--- src/lectures/repository/registrationHistory/registrationHistory.repository.interface.ts
+++ src/lectures/repository/registrationHistory/registrationHistory.repository.interface.ts
@@ -1,11 +1,23 @@
-import { RegistrationHistory } from "../../entity/registrationHistory.entity";
+import { RegistrationHistoriesDomain } from "../../domain/registrationHistories.domain";
 
 export const IREGISTRATION_HISTORY_REPOSITORY =
   "IRegistrationHistoryRepository";
 
 export interface IRegistrationHistoryRepository {
-  findByUserIdAndDate(
+  findByUserIdAndLectureIdAndDate(
     userId: number,
+    lectureId: number,
     registrationDate: string,
-  ): Promise<RegistrationHistory[]>;
+  ): Promise<RegistrationHistoriesDomain>;
+
+  findByLectureIdAndDate(
+    lectureId: number,
+    registrationDate: string,
+  ): Promise<RegistrationHistoriesDomain>;
+
+  insert(
+    userId: number,
+    lectureId: number,
+    registrationDate: string,
+  ): Promise<boolean>;
 }
--- src/lectures/repository/registrationHistory/registrationHistory.repository.ts
+++ src/lectures/repository/registrationHistory/registrationHistory.repository.ts
@@ -1,8 +1,11 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { IRegistrationHistoryRepository } from "./registrationHistory.repository.interface";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { RegistrationHistory } from "../../entity/registrationHistory.entity";
+import { RegistrationHistoriesDomain } from "../../domain/registrationHistories.domain";
+import { RegistrationHistoriesMapper } from "../../mapper/registrationHistories.mapper";
+import { LectureSchedule } from "../../entity/lectureSchedule.entity";
 
 @Injectable()
 export class RegistrationHistoryRepositoryImpl
@@ -11,22 +14,91 @@ export class RegistrationHistoryRepositoryImpl
   constructor(
     @InjectRepository(RegistrationHistory)
     private readonly registrationHistory: Repository<RegistrationHistory>,
+    @InjectRepository(LectureSchedule)
+    private readonly lectureSchedule: Repository<LectureSchedule>,
   ) {}
 
-  async findByUserIdAndDate(
+  async findByUserIdAndLectureIdAndDate(
     userId: number,
+    lectureId: number,
     registrationDate: string,
-  ): Promise<RegistrationHistory[]> {
-    return await this.registrationHistory.find({
+  ): Promise<RegistrationHistoriesDomain> {
+    const registrationHistoryList = await this.registrationHistory.find({
       relations: {
-        lectureSchedule: true,
+        lectureSchedule: {
+          lecture: true,
+        },
       },
       where: {
         userId: userId,
         lectureSchedule: {
           lectureDate: registrationDate,
+          lecture: {
+            id: lectureId,
+          },
         },
       },
     });
+
+    return RegistrationHistoriesMapper.toDomain(registrationHistoryList);
+  }
+
+  async findByLectureIdAndDate(
+    lectureId: number,
+    registrationDate: string,
+  ): Promise<RegistrationHistoriesDomain> {
+    const registrationHistoryList = await this.registrationHistory.find({
+      relations: {
+        lectureSchedule: { lecture: true },
+      },
+      where: {
+        status: true,
+        lectureSchedule: {
+          lectureDate: registrationDate,
+          lecture: {
+            id: lectureId,
+          },
+        },
+      },
+    });
+
+    return RegistrationHistoriesMapper.toDomain(registrationHistoryList);
+  }
+
+  async insert(
+    userId: number,
+    lectureId: number,
+    registrationDate: string,
+  ): Promise<boolean> {
+    const lectureSchedule = await this.lectureSchedule.findOne({
+      relations: {
+        lecture: true,
+      },
+      where: {
+        lectureDate: registrationDate,
+        lecture: {
+          id: lectureId,
+        },
+      },
+    });
+
+    if (!lectureSchedule) {
+      throw new BadRequestException("해당 하는 날짜에 강의가 없습니다.");
+    }
+
+    await this.registrationHistory
+      .createQueryBuilder()
+      .insert()
+      .into(RegistrationHistory)
+      .values([
+        {
+          userId: userId,
+          status: true,
+          lectureSchedule: lectureSchedule,
+        },
+      ])
+      .execute();
+
+    return true;
   }
 }
